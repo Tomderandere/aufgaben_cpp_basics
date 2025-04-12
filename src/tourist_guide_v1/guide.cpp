f3405ea54d68676ffacc947a8d86d1c998806066
Tomderandere <tompbickle@gmail.com>
2025-04-12 12:22:51 +0200

    guide

--- src/tourist_guide_v1/guide.cpp
+++ src/tourist_guide_v1/guide.cpp
@@ -4,7 +4,15 @@ namespace guide {
 std::pair<std::string, int> get_place_info(
     std::vector<std::string> places, std::vector<std::string> descriptions,
     std::vector<int> entry_fees, std::string place) {
-  // TODO
+  for (int i = 0; i < places.size(); i++)
+  {
+    if (places[i]==place)
+    {
+      return {descriptions[i], entry_fees[i]};
+    }
+    
+  }
+  
   return {"", -1};
 }
 }  // namespace guide
